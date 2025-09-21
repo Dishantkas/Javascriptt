a7b23482391e4990aa0e23e18b4b7723a4af81c9
Dishantkas <dishantdhiman2014@gmail.com>
2025-09-21 12:30:11 +0530

    Start

---
+++ test.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!");
+const accountID =  13322460;
+let accountEmail = "dd";
+var accountPassword = "1234";
+accountCity = "Santiago";
+
+// accountID = 123;
+// console.log(accountID);
+
+
+/* Prefer not to use var, use let instead */  
+accountEmail = "ss";
+accountPassword = "5678";
+accountCity = "Valparaiso";
+
+console.log([accountEmail, accountPassword, accountCity, accountID]);
