fc75c8f2827ff2902382d362de6c91d7f85d9c2b
Dishantkas <dishantdhiman2014@gmail.com>
2025-09-21 14:33:05 +0530

    11

---
+++ DatatypeAndECMA.js
@@ -0,0 +1,14 @@
+let name = "John"; //string
+let age = 30; //number
+let isStudent = false; //boolean
+let height = null; //standalone value
+let weight = undefined; // not defined the value
+
+// these all are primitives data types in JS
+
+// object
+
+console.log(typeof name);
+
+// null is an object in JS
+
